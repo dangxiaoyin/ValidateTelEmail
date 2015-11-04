5145b36254d7ef1535af5c4b815a6a21e6b33417
dangxy <945979125@qq.com>
2015-11-04 16:37:52 +0800

    验证手机号 邮箱

--- ValidateTelEmail/ViewController.m
+++ ValidateTelEmail/ViewController.m
@@ -17,7 +17,130 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    BOOL email = [ViewController isValidateEmail:@"12345678@qq.com"];
+    
+    NSLog(@"EMAIL:%d",email);
+    
+    BOOL tel = [ViewController isValidateTelNumber:@"15951991234"];
+    
+    NSLog(@"TEL:%d",tel);
+    
+    [self isMobileNumber:@"15951991234"];
+    
 }
+
+
+
+//验证电话号码
+
++(BOOL)isValidateTelNumber:(NSString *)number {
+    
+    NSString *strRegex = @"[0-9]{11,11}";
+    
+    BOOL rt = [self isValidateRegularExpression:number byExpression:strRegex];
+    
+    return rt;
+    
+}
+
+
+//验证email
+
++(BOOL)isValidateEmail:(NSString *)email {
+    //jfkjkfj  @  ddvv  . com
+    NSString *strRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,5}";
+    BOOL rt = [self isValidateRegularExpression:email byExpression:strRegex];
+    
+    return rt;
+    
+}
+
+
+// 正则判断手机号码地址格式
+
+- (BOOL)isMobileNumber:(NSString *)mobileNum
+{
+    /**
+     * 手机号码
+     * 移动：134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188
+     * 联通：130,131,132,152,155,156,185,186
+     * 电信：133,1349,153,180,189
+     */
+    NSString * MOBILE = @"^1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$";
+    /**
+     10         * 中国移动：China Mobile
+     11         * 134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188
+     12         */
+    NSString * CM = @"^1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$";
+    /**
+     15         * 中国联通：China Unicom
+     16         * 130,131,132,152,155,156,185,186
+     17         */
+    NSString * CU = @"^1(3[0-2]|5[256]|8[56])\\d{8}$";
+    /**
+     20         * 中国电信：China Telecom
+     21         * 133,1349,153,180,189
+     22         */
+    NSString * CT = @"^1((33|53|8[09])[0-9]|349)\\d{7}$";
+    /**
+     25         * 大陆地区固话及小灵通
+     26         * 区号：010,020,021,022,023,024,025,027,028,029
+     27         * 号码：七位或八位
+     28         */
+    // NSString * PHS = @"^0(10|2[0-5789]|\\d{3})\\d{7,8}$";
+    
+    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
+    NSPredicate *regextestcm = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM];
+    NSPredicate *regextestcu = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU];
+    NSPredicate *regextestct = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT];
+    
+    if (([regextestmobile evaluateWithObject:mobileNum])
+        || ([regextestcm evaluateWithObject:mobileNum])
+        || ([regextestct evaluateWithObject:mobileNum])
+        || ([regextestcu evaluateWithObject:mobileNum]))
+    {
+        if([regextestcm evaluateWithObject:mobileNum]) {
+            
+            NSLog(@"CHINA MOBILE : 中国移动");
+        }
+        else if([regextestct evaluateWithObject:mobileNum]) {
+            
+            NSLog(@"CHINA TELECOM : 中国电信");
+        }
+        else if ([regextestcu evaluateWithObject:mobileNum]) {
+            
+            NSLog(@"CHINA UNICOM : 中国联通");
+        }
+        else {
+            
+            NSLog(@"UNKNOWN : 未知数字");
+        }
+        
+        return YES;
+    }
+    else
+    {
+        return NO;
+    }
+}
+
+
+
+//是否是有效的正则表达式
+
++(BOOL)isValidateRegularExpression:(NSString *)strDestination byExpression:(NSString *)strExpression
+
+{
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", strExpression];
+    
+    return [predicate evaluateWithObject:strDestination];
+    
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
